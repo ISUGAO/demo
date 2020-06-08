dd045a45ccdef1d5162be6795250568600031e02
gaolijie <1171186399@qq.com>
2020-06-08 15:26:26 +0800

    DOCKERFILE

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
-    @GetMapping("test")
+    @GetMapping("gc")
     public String test(){
         return "success";
     }
